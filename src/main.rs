b98c6b250eafbbd714b2d2d6ce9105ec2ea138f3
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-11-30 14:23:53 +0100

    new main for debug purposes

--- src/main.rs
+++ src/main.rs
@@ -8,36 +8,39 @@ struct Velocity {
     dx: f32,
     dy: f32,
 }
+struct Foo;
 
-fn move_system<'a>(mut query: Query<'a, (&'a mut Position, &'a mut Velocity)>) {
-    // let mut query = entities.query::<(&mut Position, &Velocity)>();
-    // let mut query: Vec<(&mut Position, &mut Velocity)> = entities.iter_mut();
+fn move_system<'a>(_: &mut SystemsContext, mut query: Query<'a, (&'a mut Position, &'a mut Velocity)>) {
     for (position, velocity) in query.iter_mut() {
         position.x += velocity.dx;
         position.y += velocity.dy;
-    }
 
-    println!("move system finished");
+        println!("Pos: ({}, {}) Vel: ({}, {})", position.x, position.y, velocity.dx, velocity.dy);
+    }
 }
 
-fn log_system(_: Query<()>) {
+fn log_system(_: &mut SystemsContext, _: Query<&Foo>) {
     println!("Log system");
 }
 
-fn debug_system(_: Query<()>) {
-    println!("Debug system");
-}
+fn startup_system(ctx: &mut SystemsContext, _: Query<()>) {
+    ctx.commands.spawn_empty()
+        .insert(Position { x: 0.0, y: 0.0 })
+        .insert(Velocity { dx: 1.0, dy: 1.0 });
 
-fn run_system(_: Query<()>) {
-    println!("Run system");
+    ctx.commands.spawn_empty()
+        .insert(Position { x: 10.0, y: 10.0 })
+        .insert(Velocity { dx: 0.1, dy: 0.1 })
+        .insert(Foo);
+
+    println!("startup system finished");
 }
 
 fn main() {
     App::build()
+        .add_startup_system(System::new("startup", startup_system))
         .add_system(System::new("move", move_system))
-        .add_system(System::new("move_2", move_system))
-        .add_system(System::new("log", log_system))
-        .add_system(System::new("debug", debug_system))
-        .add_system(System::new("run", run_system))
+        // .add_system(System::new("move_2", move_system))
+        // .add_system(System::new("log", log_system))
         .run();
 }
