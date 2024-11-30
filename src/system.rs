2eca6f0b5229fe0724f6ad00ab341e65f78ab726
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-11-30 02:58:38 +0100

    feat(system): implement SystemsContext

--- src/system.rs
+++ src/system.rs
@@ -1,24 +1,38 @@
-use crate::{query::Query, world::World};
+use crate::{entities::Entities, prelude::Commands, query::Query, resources::Resources};
+
+pub struct SystemsContext<'a> {
+    pub commands: Commands,
+    pub resources: &'a mut Resources,
+}
+
+impl<'a> SystemsContext<'a> {
+    pub fn new(commands: Commands, resources: &'a mut Resources) -> Self {
+        Self {
+            commands,
+            resources,
+        }
+    }
+}
 
 pub struct System {
     name: String,
     func_ptr: *const (),
-    exec: Box<dyn FnMut(&mut World)>,
+    exec: Box<dyn FnMut(&mut SystemsContext, &mut Entities)>,
 }
 
 impl System {
-    pub fn new<T: 'static>(name: &str, func: fn(Query<T>)) -> System {
+    pub fn new<T: 'static>(name: &str, func: fn(&mut SystemsContext, Query<T>)) -> System {
         System {
             name: name.to_string(),
             func_ptr: func as *const (),
-            exec: Box::new(move |world| {
-                let query = Query::new(world);
-                func(query);
+            exec: Box::new(move |ctx, entities| {
+                let query = Query::new(entities);
+                func(ctx, query);
             }),
         }
     }
 
-    pub fn run(&mut self, world: &mut World) {
-        (self.exec)(world);
+    pub fn run(&mut self, ctx: &mut SystemsContext, entities: &mut Entities) {
+        (self.exec)(ctx, entities);
     }
 }
