40715e500e338b14c952d90448e48888485e7edf
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-12-02 17:38:14 +0100

    refactor: change some pub apis to pub(crate)

--- src/world/entities.rs
+++ src/world/entities.rs
@@ -19,146 +19,8 @@ impl Sub<u32> for EntityId {
     }
 }
 
-// has to be public for now because of query.rs
 #[derive(Debug)]
-pub struct Archetype {
-    entity_ids: Vec<EntityId>,
-    types: HashMap<TypeId, usize>,
-    pub components: Vec<Vec<Box<dyn Any>>>,
-}
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
-pub struct ArchetypeId(u64);
-
-impl Archetype {
-    pub fn new(types: Vec<TypeId>) -> Self {
-        let original_len = types.len();
-        let types = Self::sort_types(types);
-        let types = types.into_iter().enumerate()
-            .map(|(i, v)| (v, i))
-            .collect::<HashMap<TypeId, usize>>();
-
-        assert!(types.len() == original_len, "Duplicate types in archetype");
-
-        let components = types.iter().map(|_| Vec::new()).collect();
-
-        Self {
-            entity_ids: Vec::new(),
-            types,
-            components,
-        }
-    }
-
-    /// Insert new entity
-    fn insert_entity(&mut self, entity_id: EntityId, components: Vec<Box<dyn Any>>) {
-        self.entity_ids.push(entity_id);
-
-        let components = components.into_iter()
-            .map(|v| ((*v).type_id(), v))
-            .collect::<Vec<_>>();
-
-        let component_types = components.iter().map(|(t, _)| *t).collect::<Vec<_>>();
-        assert!(self.has_types_all(&component_types), "Component types mismatch with archetype types");
-
-        for (type_id, component) in components {
-            let index = self.types[&type_id];
-            self.components[index].push(component);
-        }
-
-        assert!(
-            self.components.iter().all(|row| row.len() == self.entity_ids.len()), 
-            "Specific component length mismatch with entity IDs length"
-        );
-        assert!(
-            self.components.len() == self.types.len(),
-            "Components length mismatch with types length"
-        );
-    }
-
-    /// Remove entity, returns removed components or None if entity_id doesn't exist
-    fn remove_entity(&mut self, entity_id: EntityId) -> Option<Vec<Box<dyn Any>>> {
-        if let Some(index) = self.entity_ids.iter().position(|id| *id == entity_id) {
-            self.entity_ids.remove(index);
-
-            let mut removed = Vec::with_capacity(self.components.len());
-            for components in self.components.iter_mut() {
-                removed.push(components.remove(index))
-            }
-
-            return Some(removed);
-        }
-
-        None
-    }
-
-    /// Update component, returns true if successful
-    fn update_component(&mut self, entity_id: EntityId, component: Box<dyn Any>) -> bool {
-        if let Some(index) = self.entity_ids.iter().position(|id| *id == entity_id) {
-            let type_id = (*component).type_id();
-            let component_index = self.types[&type_id];
-            self.components[component_index][index] = component;
-            return true
-        }
-        false
-    }
-
-    /// Returns sorted types
-    pub fn sort_types(mut types: Vec<TypeId>) -> Vec<TypeId> {
-        types.sort_by(|a, b| a.cmp(b));
-        types
-    }
-
-    /// Amount of entities in this archetype
-    pub fn len(&self) -> usize {
-        self.entity_ids.len()
-    }
-
-    /// Exposes types hashmap
-    pub fn types(&self) -> &HashMap<TypeId, usize> {
-        &self.types
-    }
-
-    /// Exposes sorted types as vector
-    pub fn types_vec(&self) -> Vec<TypeId> {
-        let types: Vec<_> = self.types.iter().map(|(k, _)| *k).collect();
-        Self::sort_types(types)
-    }
-
-    /// Check if type_id exists in self
-    pub fn has_type(&self, type_id: TypeId) -> bool {
-        self.types.contains_key(&type_id)
-    }
-
-    /// Check if all type_ids exist in self
-    pub fn has_types(&self, type_ids: &[TypeId]) -> bool {
-        type_ids.iter().all(|type_id| self.has_type(*type_id))
-    }
-
-    /// Check if all type_ids exist in self, no more no less
-    pub fn has_types_all(&self, type_ids: &[TypeId]) -> bool {
-        self.types.len() == type_ids.len() && self.has_types(type_ids)
-    }
-
-    /// Same as has_type but with generic T type
-    pub fn has_t<T: 'static>(self) -> bool {
-        self.has_type(TypeId::of::<T>())
-    }
-
-    /// Returns hash of sorted types
-    pub fn hash_types(types: Vec<TypeId>) -> ArchetypeId {
-        let mut hasher = DefaultHasher::new();
-        let types = Self::sort_types(types);
-
-        for type_id in types {
-            type_id.hash(&mut hasher);
-        }
-
-        let hash = hasher.finish();
-        ArchetypeId(hash)
-    } 
-}
-
-#[derive(Debug)]
-pub struct Entities {
+pub(crate) struct Entities {
     next_entity_id: EntityId,
     archetypes: HashMap<ArchetypeId, Archetype>, // Map archetype ID to its storage
 }
@@ -171,25 +33,25 @@ impl Entities {
         }
     }
 
-    /// Returns archetypes containing type_ids  
-    pub fn archetypes_filtered(&mut self, type_ids: &[TypeId]) -> Vec<&mut Archetype> {
-        self.archetypes.values_mut().filter(|a| a.has_types(type_ids)).collect()
-    }
-
-    /// Exposes next entity ID
-    pub fn next_entity_id(&self) -> EntityId {
-        self.next_entity_id
-    }
-
     /// Step next entity ID counter
     /// Returns new entity ID
-    pub fn step_entity_id(&mut self) -> EntityId {
+    fn step_entity_id(&mut self) -> EntityId {
         self.next_entity_id = self.next_entity_id + 1;
         self.next_entity_id
     }
 
+    /// Returns archetypes containing type_ids  
+    pub(crate) fn archetypes_filtered(&mut self, type_ids: &[TypeId]) -> Vec<&mut Archetype> {
+        self.archetypes.values_mut().filter(|a| a.has_types(type_ids)).collect()
+    }
+
+    /// Exposes next entity ID
+    pub(crate) fn next_entity_id(&self) -> EntityId {
+        self.next_entity_id
+    }
+
     /// Spawn new entity with components
-    pub fn spawn_entity(&mut self, entity_id: EntityId, components: Vec<Box<dyn Any>>) {
+    pub(crate) fn spawn_entity(&mut self, entity_id: EntityId, components: Vec<Box<dyn Any>>) {
         let types = components.iter().map(|c| (**c).type_id()).collect::<Vec<_>>();
         let archetype_id = Archetype::hash_types(types.clone()); 
 
@@ -206,14 +68,14 @@ impl Entities {
     }
 
     /// Despawn entity
-    pub fn despawn_entity(&mut self, entity_id: EntityId) {
+    pub(crate) fn despawn_entity(&mut self, entity_id: EntityId) {
         if let Some(archetype) = self.archetypes.values_mut().find(|a| a.entity_ids.contains(&entity_id)) {
             archetype.remove_entity(entity_id);
         }
     }
 
     /// Insert new component
-    pub fn insert_component(&mut self, entity_id: EntityId, component: Box<dyn Any>) {
+    pub(crate) fn insert_component(&mut self, entity_id: EntityId, component: Box<dyn Any>) {
         if let Some(archetype) = self.archetypes.values_mut().find(|a| a.entity_ids.contains(&entity_id)) {
             let type_id = (*component).type_id(); 
             if archetype.has_type(type_id) {
@@ -235,7 +97,7 @@ impl Entities {
     }
 
     /// Remove component
-    pub fn remove_component(&mut self, entity_id: EntityId, type_id: TypeId) {
+    pub(crate) fn remove_component(&mut self, entity_id: EntityId, type_id: TypeId) {
         if let Some(archetype) = self.archetypes.values_mut().find(|a| a.entity_ids.contains(&entity_id)) {
             if !archetype.has_type(type_id) {
                 return;
