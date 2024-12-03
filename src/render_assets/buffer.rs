4d1b7b6c72a871f8440c65e18903c9853afbcd3e
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-12-03 23:45:46 +0100

    feat(buffer): implement buffer render asset WIP

---
+++ src/render_assets/buffer.rs
@@ -0,0 +1,64 @@
+use bytemuck::{AnyBitPattern, NoUninit};
+use wgpu::util::DeviceExt;
+
+pub struct Buffer {
+    pub label: String,
+    pub vertex: Option<wgpu::Buffer>,
+    pub index: Option<wgpu::Buffer>,
+    pub uniform: Option<wgpu::Buffer>,
+    pub num_indices: u32,
+}
+
+impl Buffer {
+    pub fn new(label: &str) -> Self {
+        Self {
+            label: label.to_string(),
+            vertex: None,
+            index: None,
+            uniform: None,
+            num_indices: 0,
+        }
+    }
+
+    pub fn create_vertex_buffer<A>(self, data: &[A], usages: Option<wgpu::BufferUsages>, device: &wgpu::Device) -> Self
+    where A: NoUninit + AnyBitPattern {
+        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some(&format!("{}_vertex_buffer", self.label)),
+            contents: bytemuck::cast_slice(data),
+            usage: if let Some(usages) = usages { wgpu::BufferUsages::VERTEX | usages } else { wgpu::BufferUsages::VERTEX }, 
+        });
+
+        Self {
+            vertex: Some(vertex_buffer),
+            ..self
+        }
+    }
+    
+    pub fn create_index_buffer(self, data: &[u32], usages: Option<wgpu::BufferUsages>, device: &wgpu::Device) -> Self {
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some(&format!("{}_index_buffer", self.label)),
+            contents: bytemuck::cast_slice(data),
+            usage: if let Some(usages) = usages { wgpu::BufferUsages::INDEX | usages } else { wgpu::BufferUsages::INDEX }, 
+        });
+
+        Self {
+            index: Some(index_buffer),
+            num_indices: data.len() as u32,
+            ..self
+        }
+    }
+
+    pub fn create_uniform_buffer<A>(self, data: &[A], usages: Option<wgpu::BufferUsages>, device: &wgpu::Device) -> Self
+    where A: NoUninit + AnyBitPattern {
+        let uniform_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some(&format!("{}_uniform_buffer", self.label)),
+            contents: bytemuck::cast_slice(data),
+            usage: if let Some(usages) = usages { wgpu::BufferUsages::UNIFORM | usages } else { wgpu::BufferUsages::UNIFORM }, 
+        });
+
+        Self {
+            vertex: Some(uniform_buffer),
+            ..self
+        }
+    }
+}
