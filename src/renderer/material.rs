89d45573c13f96cc2104d6feea11a90e5fc88e04
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-12-03 23:46:58 +0100

    feat(material): implement RenderAsset for material

--- src/renderer/material.rs
+++ src/renderer/material.rs
@@ -1,4 +1,4 @@
-use crate::assets::Handle;
+use crate::{assets::Handle, prelude::Resources, render_assets::{BindGroup, Buffer, Pipeline, RenderAsset, RenderAssets}, resources};
 
 use super::{Color, Face, Image};
 
@@ -19,6 +19,28 @@ pub struct Material {
     pub unlit: bool,
 }
 
+impl Material {
+    fn uniform_data(&self) -> Vec<u8> {
+        let mut data = Vec::new();
+
+        data.extend_from_slice(bytemuck::bytes_of(&self.base_color));
+        data.extend_from_slice(bytemuck::bytes_of(&self.emissive));
+        data.extend_from_slice(bytemuck::cast_slice(&[
+            self.emissive_exposure_weight,
+            self.perceptual_roughness,
+            self.metallic,
+            self.reflectance,
+        ]));
+        data.extend_from_slice(bytemuck::cast_slice(&[
+            self.flip_normal_map_y,
+            matches!(self.cull_mode, Some(Face::Back)),
+            self.unlit
+        ]));
+
+        data
+    }
+}
+
 impl Default for Material {
     fn default() -> Self {
         Self {
@@ -34,5 +56,34 @@ impl Default for Material {
             cull_mode: Some(Face::Back),
             unlit: false,
         } 
+    }
+}
+
+impl RenderAsset<Buffer> for Material {
+    fn create_render_asset(&self, device: &wgpu::Device, _: &mut Resources) -> Buffer {
+        let buffer = Buffer::new("material")
+            .create_uniform_buffer(&self.uniform_data(), None, device);
+        
+        buffer
+    }
+}
+
+impl RenderAsset<BindGroup> for Material {
+    fn create_render_asset(&self, device: &wgpu::Device, resources: &mut Resources) -> BindGroup {
+        BindGroup::build("material", device)
+            .add_texture(&self.base_color_texture, resources)
+            .add_texture(&self.normal_map_texture, resources)
+            // .add_uniform(self, device)
+            .finish()
+    }
+}
+
+impl RenderAsset<Pipeline> for Material {
+    fn create_render_asset(&self, device: &wgpu::Device, resources: &mut Resources) -> Pipeline {
+        // Pipeline::build("material", device)        
+
+
+
+        todo!()
     }
 }
