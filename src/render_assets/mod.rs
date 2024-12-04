2f242003722dead1f3807e484d700e215c2b8737
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-12-04 02:20:27 +0100

    feat(handle): implement handle for render assets

--- src/render_assets/mod.rs
+++ src/render_assets/mod.rs
@@ -2,8 +2,10 @@ mod render_assets;
 mod buffer;
 mod bind_group;
 mod pipeline;
+mod render_handle;
 
 pub use render_assets::{RenderAssets, RenderAsset};
 pub use buffer::Buffer;
 pub use bind_group::BindGroup;
 pub use pipeline::Pipeline;
+pub use render_handle::RenderHandle;
---
+++ src/render_assets/render_handle.rs
@@ -0,0 +1,45 @@
+use std::{fmt::Debug, hash::Hash};
+
+/// Handle to a render asset resource
+pub struct RenderHandle<T> {
+    id: u64,
+    _marker: std::marker::PhantomData<T>,
+}
+
+impl<T> RenderHandle<T> {
+    pub(super) fn new(id: u64) -> Self {
+        Self {
+            id,
+            _marker: std::marker::PhantomData
+        }
+    }
+
+    pub(crate) fn id(&self) -> u64 {
+        self.id
+    }
+}
+
+impl<T> Hash for RenderHandle<T> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.id.hash(state);
+    }
+}
+
+impl<T> Debug for RenderHandle<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "RenderHandle({})", self.id)
+    }
+}
+
+impl<T> Clone for RenderHandle<T> {
+    fn clone(&self) -> Self {
+        Self::new(self.id)
+    }
+}
+
+impl<T> PartialEq for RenderHandle<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+impl<T> Eq for RenderHandle<T> {}
