7b95ab341881c144c28349b80d49b660302f4d95
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-12-04 02:21:15 +0100

    refactor(renderassets): implement normal storage, and storage linked to handle<T>

--- src/render_assets/render_assets.rs
+++ src/render_assets/render_assets.rs
@@ -2,35 +2,51 @@ use std::{any::TypeId, collections::HashMap};
 
 use crate::{assets::{Assets, Handle}, prelude::Resources};
 
+use super::RenderHandle;
+
 pub trait RenderAsset<R> {
     fn create_render_asset(&self, device: &wgpu::Device, resources: &mut Resources) -> R;
 }
 
+/// Generic handle for Asset of any type
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct AssetHandleId(TypeId, u64);
+
+impl<T: 'static> Into<AssetHandleId> for &Handle<T> {
+    fn into(self) -> AssetHandleId {
+        AssetHandleId(TypeId::of::<T>(), self.id())
+    }
+}
+
 pub struct RenderAssets<T> {
-    storage: HashMap<(TypeId, u64), T>,
+    storage: HashMap<RenderHandle<T>, T>,
+    handle_map: HashMap<AssetHandleId, RenderHandle<T>>,
+    next_id: u64,
 }
 
 impl<T> RenderAssets<T> {
     pub fn new() -> Self {
         Self {
             storage: HashMap::new(),
+            handle_map: HashMap::new(),
+            next_id: 0,
         }
     }
 
-    // fn step_id(&mut self) -> Handle<T> {
-    //     let id = self.next_id;
-    //     self.next_id += 1;
-    //     Handle::new(id)
-    // }
-    //
-    // pub fn add(&mut self, asset: T) -> Handle<T> {
-    //     let id = self.step_id();
-    //     self.storage.insert(id.clone(), asset);
-    //     id
-    // }
+    fn step_id(&mut self) -> RenderHandle<T> {
+        let id = self.next_id;
+        self.next_id += 1;
+        RenderHandle::new(id)
+    }
 
-    fn get_key<A: 'static>(handle: &Handle<A>) -> (TypeId, u64) {
-        (TypeId::of::<A>(), handle.id())
+    pub fn insert(&mut self, asset: T) -> RenderHandle<T> {
+        let id = self.step_id();
+        self.storage.insert(id.clone(), asset);
+        id
+    }
+
+    pub fn get(&self, handle: &RenderHandle<T>) -> Option<&T> {
+        self.storage.get(&handle)
     }
 
     pub fn get_or_create<A>(
@@ -40,9 +56,18 @@ impl<T> RenderAssets<T> {
         resources: &mut Resources
     ) -> &T
     where A: 'static + RenderAsset<T> {
-        self.storage
-            .entry(Self::get_key(handle))
-            .or_insert_with(|| Self::create_asset(handle, device, resources))
+        match self.handle_map.get(&handle.into()){
+            Some(key) => {
+                self.storage
+                    .entry(key.clone())
+                    .or_insert_with(|| Self::create_asset(handle, device, resources))
+            },
+            None => {
+                let key = self.insert(Self::create_asset(handle, device, resources));
+                self.handle_map.insert(handle.into(), key.clone());
+                self.storage.get(&key).unwrap()
+            }
+        }
     }
 
     fn create_asset<A>(handle: &Handle<A>, device: &wgpu::Device, resources: &mut Resources) -> T
@@ -55,6 +80,8 @@ impl<T> RenderAssets<T> {
     }
 
     pub fn remove<A: 'static>(&mut self, handle: &Handle<A>) -> Option<T> {
-        self.storage.remove(&Self::get_key(handle))
+        // TODO: should we remove both the handle and the asset? 
+        let key = self.handle_map.remove(&handle.into())?;
+        self.storage.remove(&key)
     }
 }
