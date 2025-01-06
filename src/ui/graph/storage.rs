1c68c7125ffbc4e9ec638a99dd0c500aeebdc944
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-01-06 18:10:04 +0100

    feat(ui_storage): implement ui transform storage wrapper

---
+++ src/ui/graph/storage.rs
@@ -0,0 +1,28 @@
+use std::ops::{Deref, DerefMut};
+
+use crate::render_assets::Storage;
+
+use super::SystemsContext;
+
+
+pub struct UiTransformStorage(Storage);
+
+impl UiTransformStorage {
+    pub fn new(n: usize, size: usize, ctx: &mut SystemsContext, visibility: wgpu::ShaderStages) -> Self {
+        Self(Storage::new("node_transform", n, size, ctx, visibility))
+    }
+}
+
+impl Deref for UiTransformStorage {
+    type Target = Storage;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for UiTransformStorage {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
