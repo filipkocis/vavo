a405353ae72e067528bf9b7a1d50e2dea9cdab2c
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-01-10 02:03:12 +0100

    feat(State): implement system for automatic state transitioning

---
+++ src/state/systems.rs
@@ -0,0 +1,32 @@
+use crate::prelude::*;
+
+use super::event::StateTransitionEvent;
+
+/// State transitioning system, one per state type. Used in the FrameEnd system stage.
+pub fn apply_state_transition<S: States + 'static>(ctx: &mut SystemsContext, _: Query<()>) {
+    let current_state = ctx.resources.get_mut::<State<S>>();     
+    let next_state = ctx.resources.get_mut::<NextState<S>>();
+
+    // resource option
+    let Some(mut next_state) = next_state else { 
+        return
+    };
+    let next_state = next_state.take();
+    // next state option
+    let Some(next_state) = next_state else {
+        return
+    };
+
+    // resource option
+    let Some(mut current_state) = current_state else { 
+        return
+    };
+
+    if current_state.get() == next_state {
+        return
+    }
+
+    // queue event and update state
+    ctx.event_writer.send(StateTransitionEvent::new(current_state.get(), next_state));
+    current_state.update(next_state);
+}
