839d06fc62f4b1232d553959b2d39ca3b9d859f5
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-01-10 02:02:16 +0100

    feat(State): implement state transition event

---
+++ src/state/event.rs
@@ -0,0 +1,23 @@
+use super::States;
+
+/// Describes a state transition
+pub struct StateTransitionEvent<S: States> {
+    pub from: S,
+    pub to: S,
+}
+
+impl<S: States> StateTransitionEvent<S> {
+    pub fn new(from: S, to: S) -> Self {
+        Self { from, to }
+    }
+
+    /// True if current state is exiting from 'state'
+    pub fn exiting(&self, state: S) -> bool {
+        self.from == state
+    }
+
+    /// True if current state is entering 'state'
+    pub fn entering(&self, state: S) -> bool {
+        self.to == state
+    }
+}
