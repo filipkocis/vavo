c835cf13755e107434ca90d63e986e27dbb8820c
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-01-24 01:50:13 +0100

    feat(asset): implement insert by handle method

--- src/assets/assets.rs
+++ src/assets/assets.rs
@@ -2,12 +2,14 @@ use std::collections::HashMap;
 
 use super::Handle;
 
+/// Storage for assets of the same type accessible by their handle
 pub struct Assets<T> {
     storage: HashMap<Handle<T>, T>,
     next_id: u64,
 }
 
 impl<T> Assets<T> {
+    /// Create new empty asset storage
     pub fn new() -> Self {
         Self {
             storage: HashMap::new(),
@@ -21,20 +23,30 @@ impl<T> Assets<T> {
         Handle::new(id)
     }
 
+    /// Adds new asset to the storage and returns its handle
     pub fn add(&mut self, asset: T) -> Handle<T> {
         let id = self.step_id();
         self.storage.insert(id.clone(), asset);
         id
     }
 
+    /// Inserts asset with the given handle, if the handle is already in use, it will be
+    /// overwritten
+    pub fn insert(&mut self, id: Handle<T>, asset: T) {
+        self.storage.insert(id, asset);
+    }
+
+    /// Get a reference to the asset
     pub fn get(&self, id: &Handle<T>) -> Option<&T> {
         self.storage.get(id)
     }
 
+    /// Get a mutable reference to the asset
     pub fn get_mut(&mut self, id: &Handle<T>) -> Option<&mut T> {
         self.storage.get_mut(id)
     }
 
+    /// Removes and returns the asset
     pub fn remove(&mut self, id: &Handle<T>) -> Option<T> {
         self.storage.remove(id)
     }
