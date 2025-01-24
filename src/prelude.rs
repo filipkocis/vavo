89ab8890f05afe1801c81a9cec3e2464448d2f9a
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-01-24 01:36:05 +0100

    feat(prelude): add crate reexports so they can be used by user

--- src/prelude.rs
+++ src/prelude.rs
@@ -6,15 +6,19 @@ pub use super::{
     world::{EntityId, Parent, Children},
     renderer::{Material, Texture, Image, Color, Face, Mesh, Meshable, shapes},
     resources::{Resources, Res, ResMut, Time, FixedTime, Timer, TimerVariant},
-    math::{Transform, GlobalTransform, camera::{Camera, Camera3D, Projection}, light::{Light, AmbientLight, DirectionalLight, PointLight, SpotLight}},
+    math::*,
     plugins::{DefaultPlugin},
     state::{State, NextState, States, StateTransitionEvent, conditions::*},
     events::{KeyboardInput, MouseInput, MouseWheel, MouseMotion, CursorMoved},
-    input::Input
+    input::Input,
 };
 
-pub use super::ui::prelude::*;
-
+/// Re-exported pallette module as color
 pub mod color {
     pub use super::super::renderer::palette::*; 
 }
+
+pub use winit::{self};
+pub use image::{self};
+pub use wgpu::{self};
+pub use glam::{self, Vec2, Vec3, Mat4, Quat};
