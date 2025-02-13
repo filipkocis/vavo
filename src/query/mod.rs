d6b7f6fb4c3051526c9930561b07b69284c0262a
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-02-13 16:04:15 +0100

    feat(query): implement QueryComponentType typeid wrapper

--- src/query/mod.rs
+++ src/query/mod.rs
@@ -1,9 +1,38 @@
 mod run;
 pub mod filter;
 
+use std::any::TypeId;
+
 pub use run::RunQuery;
 
 use crate::ecs::entities::Entities;
+
+/// Holds different types of requested [`component`](crate::ecs::components::Component) types in a query. Used to differentiate between normal
+/// references and `Option<Component>`.
+pub(crate) enum QueryComponentType {
+    /// Can be `&Component`, `&mut Component` or owned `EntityId`.
+    Normal(TypeId),
+    /// Represents the inner type of either `Option<&Component>` or `Option<&mut Component>`.
+    Option(TypeId),
+}
+
+impl QueryComponentType {
+    /// True if component was requested as an `Option<T>`.
+    pub fn is_option(&self) -> bool {
+        match self {
+            QueryComponentType::Option(_) => true,
+            _ => false,
+        }
+    }
+
+    /// Returns the underlying [`component`](crate::ecs::components::Component) type id.
+    pub fn get_inner_type(&self) -> &TypeId {
+        match self {
+            QueryComponentType::Normal(inner) => inner,
+            QueryComponentType::Option(inner) => inner,
+        }
+    }
+}
 
 pub struct Query<T, F = ()> {
     /// World's entities raw pointer to bypass lifetime limitations.
