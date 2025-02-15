d671d6e1ceb1e5d43a1d331a90201dadd8a8a337
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-02-15 23:13:32 +0100

    feat(audio): implement Sound

---
+++ src/audio/sound.rs
@@ -0,0 +1,34 @@
+use kira::sound::static_sound::StaticSoundHandle;
+
+use super::AudioCommand;
+
+/// A sound which may or may not be currently playing
+pub(crate) struct Sound(pub(crate) StaticSoundHandle);
+
+pub type PlaybackState = kira::sound::PlaybackState;
+
+impl Sound {
+    pub fn new(handle: StaticSoundHandle, commands: Vec<AudioCommand>) -> Self {
+        let mut sound = Self(handle);
+        commands.into_iter().for_each(|command| sound.apply(command));
+        sound
+    }
+
+    /// Returns the current playback state of the sound
+    pub fn state(&self) -> PlaybackState {
+        self.0.state()
+    }
+
+    fn apply(&mut self, command: AudioCommand) {
+        match command {
+            AudioCommand::Play(..) => panic!("Play command is not valid for a sound"),
+            AudioCommand::Pause(tween) => self.0.pause(tween),
+            AudioCommand::Resume(tween) => self.0.resume(tween),
+            AudioCommand::Stop(tween) => self.0.stop(tween),
+            AudioCommand::SetVolume(volume, tween) => self.0.set_volume(volume, tween),
+            AudioCommand::SetPanning(panning, tween) => self.0.set_panning(panning, tween),
+            AudioCommand::SetPlaybackRate(rate, tween) => self.0.set_playback_rate(rate, tween),
+            AudioCommand::SetLoopRegion(region) => self.0.set_loop_region(region),
+        }
+    }
+}
