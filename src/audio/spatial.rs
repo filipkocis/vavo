4649204dcda152c9854080b9fa1bc362f292f446
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-02-17 18:51:42 +0100

    feat: implement SpatialEmitter

--- src/audio/spatial.rs
+++ src/audio/spatial.rs
@@ -1,10 +1,10 @@
-use std::time::Duration;
+use std::collections::VecDeque;
 
-use kira::{listener::{ListenerHandle, ListenerId}, Tween};
+use kira::{listener::{ListenerHandle, ListenerId}, sound::IntoOptionalRegion};
 
 use crate::prelude::*;
 
-use super::{AudioManager, SystemsContext};
+use super::commands::AudioCommand;
 
 /// A marker component used to specify which [`entity`](EntityId) is the spatial listener, it's not inserted
 /// automatically, you have to insert it manually. Most likely you will want to attach it to the
@@ -25,26 +25,64 @@ impl SpatialListener {
     }
 }
 
-/// System that updates or initializes the [`spatial listener`](SpatialListener)'s position and orientation.
-pub(crate) fn update_spatial_listener(
-    ctx: &mut SystemsContext,
-    mut query: Query<(&GlobalTransform, &mut SpatialListener), (With<Camera>, Changed<GlobalTransform>)>
-) {
-    let mut manager = ctx.resources.get_mut::<AudioManager>().unwrap();
+/// Component which makes an entity a spatial audio emitter. You can play as many sounds as you
+/// want. They are attached to the main [`AudioTrack`].
+///
+/// Despawning the entity or removing the component will stop all sounds.
+#[derive(Component, Default, Debug)]
+pub struct SpatialEmitter {
+    pub(crate) commands: VecDeque<AudioCommand>,
+    // pub(crate) track: Option<SpatialTrackHandle>,
+}
 
-    for (transform, listener) in query.iter_mut() {
-        let position = transform.translation();
-        let orientation = transform.rotation();
+impl SpatialEmitter {
+    pub fn new() -> Self {
+        Self::default()
+    }
 
-        if let Some(ref mut listener) = listener.0 {
-            let instant_tween = Tween {
-                duration: Duration::ZERO,
-                ..Tween::default()
-            };
-            listener.set_position(position, instant_tween);
-            listener.set_orientation(orientation, Tween::default());
-        } else {
-            listener.0 = Some(manager.add_listener(position, orientation).expect("Failed to add spatial listener"))
-        }
+    /// Pushes a command to the queue 
+    fn push(&mut self, command: AudioCommand) -> &mut AudioCommand {
+        self.commands.push_back(command);
+        self.commands.back_mut().unwrap()
+    }
+
+    /// Spatially plays an audio asset
+    pub fn play(&mut self, source: Handle<AudioSource>) -> PlayCommand {
+        self.push(AudioCommand::Play(source, Default::default())).play_command()
+    }
+
+    /// Stops all spatial sounds
+    pub fn stop(&mut self) -> TweenCommand {
+        self.push(AudioCommand::Stop(Default::default())).tween_command()
+    }
+
+    /// Pauses all spatial sounds
+    pub fn pause(&mut self) -> TweenCommand {
+        self.push(AudioCommand::Pause(Default::default())).tween_command()
+    }
+
+    /// Resumes all spatial sounds
+    pub fn resume(&mut self) -> TweenCommand {
+        self.push(AudioCommand::Resume(Default::default())).tween_command()
+    }
+
+    /// Sets the volume of all spatial sounds in decibels
+    pub fn set_volume(&mut self, volume: f32) -> TweenCommand {
+        self.push(AudioCommand::SetVolume(volume, Default::default())).tween_command()
+    }
+
+    /// Sets the panning of all spatial sounds
+    pub fn set_panning(&mut self, panning: f32) -> TweenCommand {
+        self.push(AudioCommand::SetPanning(panning, Default::default())).tween_command()
+    }
+
+    /// Sets the playback rate of all spatial sounds
+    pub fn set_playback_rate(&mut self, rate: f64) -> TweenCommand {
+        self.push(AudioCommand::SetPlaybackRate(rate, Default::default())).tween_command()
+    }
+
+    /// Sets the loop region of all spatial sounds
+    pub fn set_loop_region(&mut self, region: impl IntoOptionalRegion) {
+        self.push(AudioCommand::SetLoopRegion(region.into_optional_region()));
     }
 }
