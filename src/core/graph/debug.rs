c57fb2b63f7370810d258faf8300b2ae35ccfe40
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-04-09 22:37:07 +0200

    feat: improve debug plugin for RenderGraph

--- src/core/graph/debug.rs
+++ src/core/graph/debug.rs
@@ -1,6 +1,11 @@
-use crate::{app::Plugin, prelude::Time, query::Query, system::{SystemStage, SystemsContext}};
+use crate::{
+    app::Plugin,
+    prelude::Time,
+    query::Query,
+    system::{SystemStage, SystemsContext},
+};
 
-/// QOL plugin to print the render graph topology 
+/// QOL plugin to print the render graph topology
 pub struct DebugRenderGraphPlugin;
 
 impl Plugin for DebugRenderGraphPlugin {
@@ -17,7 +22,6 @@ impl Plugin for DebugRenderGraphPlugin {
     }
 }
 
-
 pub fn print_render_graph_topology(ctx: &mut SystemsContext) {
     println!("Render Graph Topology:");
 
@@ -26,11 +30,37 @@ pub fn print_render_graph_topology(ctx: &mut SystemsContext) {
 
     println!("\nUnsorted Graph Nodes:");
     for (i, node) in graph.nodes.iter().enumerate() {
-        println!("  ({i}) {} -> {:?}", node.0, node.1.dependencies);
+        println!("({i}) {}", node.0);
+        if !node.1.after.is_empty() {
+            println!("  After -> {:?}", node.1.after);
+        }
+        if !node.1.before.is_empty() {
+            println!("  Before -> {:?}", node.1.before);
+        }
     }
 
+    let normalized = graph.normalize_dependencies();
+    let mut names = Vec::new();
+
     println!("\nSorted Graph Nodes:");
-    for (i, node) in graph.topological_sort_mut().into_iter().enumerate() {
-        println!("  ({i}) {} -> {:?}", node.name, node.dependencies);
+    for (i, node) in graph.sorted.iter().enumerate() {
+        let node = unsafe { &mut **node };
+        names.push(node.name.clone());
+
+        println!("({i}) {}", node.name);
+        if let Some(deps) = normalized.get(&node.name) {
+            if !deps.is_empty() {
+                println!("  AfterN -> {:?}", deps);
+            }
+        }
+        if !node.after.is_empty() {
+            println!("  After -> {:?}", node.after);
+        }
+        if !node.before.is_empty() {
+            println!("  Before -> {:?}", node.before);
+        }
     }
+
+    println!("\nGraph Nodes in Sequence:");
+    println!("  {}", names.join(" -> "));
 }
