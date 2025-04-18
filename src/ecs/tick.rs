36b1f222a5e619a8747612680a8740cade003cda
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-04-18 19:30:25 +0200

    feat(tick): implement Ticks

--- src/ecs/tick.rs
+++ src/ecs/tick.rs
@@ -1,5 +1,6 @@
 #[repr(transparent)]
-#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Copy, Clone, Default, Debug, PartialEq, Eq, PartialOrd, Ord)]
+/// A tick is a simple counter used to track the number of updates or frames in a game loop.
 pub struct Tick {
     value: usize,
 }
@@ -24,14 +25,52 @@ impl Tick {
     pub fn increment(&mut self) {
         self.value += 1;
     }
+}
 
+/// A struct that holds the tick values for Changed and Added filters
+pub struct Ticks {
+    changed: *mut Tick,
+    added: *const Tick,
+    current: Tick,
+}
+
+impl Ticks {
     #[inline]
-    pub fn as_ptr(&self) -> *const Tick {
-        self as _
+    pub fn new(changed: &mut Tick, added: &Tick, current: Tick) -> Self {
+        Self {
+            changed,
+            added,
+            current
+        }
     }
 
+    /// Returns the tick of the last change.
     #[inline]
-    pub fn as_mut(&mut self) -> *mut Tick {
-        self as _
+    pub fn changed(&self) -> usize {
+        unsafe { *self.changed }.get()
+    }
+
+    /// Returns the tick when the value was added.
+    #[inline]
+    pub fn added(&self) -> usize {
+        unsafe { *self.added }.get()
+    }
+
+    /// Marks the changed tick as the current tick.
+    #[inline]
+    pub fn mark_changed(&mut self) {
+        unsafe { *self.changed = self.current };
+    }
+
+    /// Checks if hte value was changed in the current tick.
+    #[inline]
+    pub fn is_changed(&self) -> bool {
+        self.changed() == self.current.get()
+    }
+
+    /// Checks if the value was added in the current tick.
+    #[inline]
+    pub fn is_added(&self) -> bool {
+        self.added() == self.current.get()
     }
 }
