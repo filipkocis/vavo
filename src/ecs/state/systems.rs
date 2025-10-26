7f01b2757c85d3c102853451ee0957d1f275e376
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-10-26 21:07:16 +0100

    impl new IntoSystem for state_transition

--- src/ecs/state/systems.rs
+++ src/ecs/state/systems.rs
@@ -1,32 +1,31 @@
-use crate::prelude::*;
+use crate::{event::event_handler::EventWriter, prelude::*};
 
 use super::event::StateTransitionEvent;
 
 /// State transitioning system, one per state type. Used in the FrameEnd system stage.
-pub fn apply_state_transition<S: States + 'static>(ctx: &mut SystemsContext, _: Query<()>) {
-    let current_state = ctx.resources.try_get_mut::<State<S>>();     
-    let next_state = ctx.resources.try_get_mut::<NextState<S>>();
-
+pub fn apply_state_transition<S: States + 'static>(
+    current_state: Option<ResMut<State<S>>>,
+    next_state: Option<ResMut<NextState<S>>>,
+    mut event_writer: EventWriter,
+) {
     // resource option
-    let Some(mut next_state) = next_state else { 
-        return
+    let Some(mut next_state) = next_state else {
+        return;
     };
     let next_state = next_state.take();
     // next state option
-    let Some(next_state) = next_state else {
-        return
-    };
+    let Some(next_state) = next_state else { return };
 
     // resource option
-    let Some(mut current_state) = current_state else { 
-        return
+    let Some(mut current_state) = current_state else {
+        return;
     };
 
     if current_state.get() == next_state {
-        return
+        return;
     }
 
     // queue event and update state
-    ctx.event_writer.send(StateTransitionEvent::new(current_state.get(), next_state));
+    event_writer.send(StateTransitionEvent::new(current_state.get(), next_state));
     current_state.update(next_state);
 }
