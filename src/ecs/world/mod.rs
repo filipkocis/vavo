70022f5918162590d3ffc78e3b2863810cfa0341
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-10-26 13:03:45 +0100

    feat: add command_queue to world (temp solution)

--- src/ecs/world/mod.rs
+++ src/ecs/world/mod.rs
@@ -1,8 +1,10 @@
 use crate::prelude::EntityId;
 use crate::query::Query;
+use crate::system::Commands;
+use crate::system::commands::CommandQueue;
 
-use super::entities::components::ComponentsRegistry;
 use super::entities::Entities;
+use super::entities::components::ComponentsRegistry;
 use super::resources::Resources;
 use super::tick::Tick;
 
@@ -13,6 +15,8 @@ pub struct World {
     pub tick: Box<Tick>,
     /// Component types metadata registry
     pub registry: ComponentsRegistry,
+
+    command_queue: CommandQueue,
 }
 
 impl Default for World {
@@ -24,6 +28,7 @@ impl Default for World {
             resources: Resources::new(),
             tick,
             registry: ComponentsRegistry::new(),
+            command_queue: CommandQueue::new(),
         };
 
         // Initialize entities
@@ -65,5 +70,22 @@ impl World {
     #[inline]
     pub fn query_filtered<T, F>(&mut self) -> Query<T, F> {
         Query::new(&mut self.entities, *self.tick)
+    }
+
+    /// Returns new commands instance with the world's command queue
+    #[inline]
+    pub fn commands<'a, 'b>(&'a mut self) -> Commands<'a, 'b>
+    where
+        'a: 'b,
+        'b: 'a,
+    {
+        Commands::new(&mut self.entities.tracking, &mut self.command_queue)
+    }
+
+    /// Flushes all queued commands to the world
+    #[inline]
+    pub(crate) fn flush_commands(&mut self) {
+        let world = unsafe { &mut *(self as *mut _) };
+        self.command_queue.apply(world)
     }
 }
