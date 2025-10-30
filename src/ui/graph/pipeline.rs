b684dcacd64f6be2bfb197ffa9fbff312885cfae
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-10-30 13:36:07 +0100

    format

--- src/ui/graph/pipeline.rs
+++ src/ui/graph/pipeline.rs
@@ -1,6 +1,6 @@
 use crate::assets::ShaderLoader;
 use crate::prelude::*;
-use crate::render_assets::{pipeline::PipelineBuilder, Pipeline};
+use crate::render_assets::{Pipeline, pipeline::PipelineBuilder};
 use crate::ui::mesh::UiMesh;
 
 pub fn create_ui_pipeline_builder(ctx: &mut SystemsContext) -> PipelineBuilder {
@@ -9,41 +9,40 @@ pub fn create_ui_pipeline_builder(ctx: &mut SystemsContext) -> PipelineBuilder {
 
     // Transform bind group layout for storage buffer
     let transform_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-        label: Some("transform_bind_group_layout"), 
-        entries: &[
-            wgpu::BindGroupLayoutEntry {
-                binding: 0,
-                visibility: wgpu::ShaderStages::VERTEX,
-                ty: wgpu::BindingType::Buffer { 
-                    ty: wgpu::BufferBindingType::Storage { read_only: true },
-                    has_dynamic_offset: false, 
-                    min_binding_size: None 
-                },
-                count: None
-            }
-        ]
+        label: Some("transform_bind_group_layout"),
+        entries: &[wgpu::BindGroupLayoutEntry {
+            binding: 0,
+            visibility: wgpu::ShaderStages::VERTEX,
+            ty: wgpu::BindingType::Buffer {
+                ty: wgpu::BufferBindingType::Storage { read_only: true },
+                has_dynamic_offset: false,
+                min_binding_size: None,
+            },
+            count: None,
+        }],
     });
 
     // Camera bind group layout for uniform buffer
     let camera_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
         label: Some("camera_bind_group_layout"),
-        entries: &[
-            wgpu::BindGroupLayoutEntry {
-                binding: 0,
-                visibility: wgpu::ShaderStages::VERTEX_FRAGMENT,
-                ty: wgpu::BindingType::Buffer { 
-                    ty: wgpu::BufferBindingType::Uniform,
-                    has_dynamic_offset: false,
-                    min_binding_size: None
-                },
-                count: None
-            }
-        ]
+        entries: &[wgpu::BindGroupLayoutEntry {
+            binding: 0,
+            visibility: wgpu::ShaderStages::VERTEX_FRAGMENT,
+            ty: wgpu::BindingType::Buffer {
+                ty: wgpu::BufferBindingType::Uniform,
+                has_dynamic_offset: false,
+                min_binding_size: None,
+            },
+            count: None,
+        }],
     });
 
     // Load shader modules
-    ctx.resources.get_mut::<ShaderLoader>()
-        .load("ui", include_str!("../../shaders/ui.wgsl"), device);
+    ctx.resources.get_mut::<ShaderLoader>().load(
+        "ui",
+        include_str!("../../shaders/ui.wgsl"),
+        device,
+    );
 
     // Create pipeline builder
     Pipeline::build("ui")
@@ -53,54 +52,57 @@ pub fn create_ui_pipeline_builder(ctx: &mut SystemsContext) -> PipelineBuilder {
         .set_fragment_shader("ui", "fs_main")
         .add_color_format(color_format)
         .set_depth_format(wgpu::TextureFormat::Depth32Float)
-        .set_push_constant_ranges(vec![
-            wgpu::PushConstantRange {
-                stages: wgpu::ShaderStages::VERTEX,
-                range: 0..4 * 2,
-            }
-        ])
+        .set_push_constant_ranges(vec![wgpu::PushConstantRange {
+            stages: wgpu::ShaderStages::VERTEX,
+            range: 0..4 * 2,
+        }])
 }
 
 pub fn create_ui_image_pipeline_builder(ctx: &mut SystemsContext) -> PipelineBuilder {
     let mut pipeline_builder = create_ui_pipeline_builder(ctx);
 
     // Image bind group layout for uniform buffer
-    let image_layout = ctx.renderer.device().create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-        label: Some("image_bind_group_layout"),
-        entries: &[
-            // texture
-            wgpu::BindGroupLayoutEntry {
-                binding: 0,
-                visibility: wgpu::ShaderStages::FRAGMENT,
-                ty: wgpu::BindingType::Texture {
-                    sample_type: wgpu::TextureSampleType::Float { filterable: true },
-                    view_dimension: wgpu::TextureViewDimension::D2,
-                    multisampled: false 
-                },
-                count: None,
-            },
-            // sampler
-            wgpu::BindGroupLayoutEntry {
-                binding: 1,
-                visibility: wgpu::ShaderStages::FRAGMENT,
-                ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
-                count: None,
-            },
-            // uniform
-            wgpu::BindGroupLayoutEntry {
-                binding: 2,
-                visibility: wgpu::ShaderStages::VERTEX_FRAGMENT,
-                ty: wgpu::BindingType::Buffer { 
-                    ty: wgpu::BufferBindingType::Uniform,
-                    min_binding_size: None,
-                    has_dynamic_offset: false,
-                },
-                count: None,
-            }
-        ]
-    });
+    let image_layout =
+        ctx.renderer
+            .device()
+            .create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                label: Some("image_bind_group_layout"),
+                entries: &[
+                    // texture
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Texture {
+                            sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                            view_dimension: wgpu::TextureViewDimension::D2,
+                            multisampled: false,
+                        },
+                        count: None,
+                    },
+                    // sampler
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+                        count: None,
+                    },
+                    // uniform
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 2,
+                        visibility: wgpu::ShaderStages::VERTEX_FRAGMENT,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Uniform,
+                            min_binding_size: None,
+                            has_dynamic_offset: false,
+                        },
+                        count: None,
+                    },
+                ],
+            });
 
-    let bind_group_layouts = pipeline_builder.bind_group_layouts.as_mut()
+    let bind_group_layouts = pipeline_builder
+        .bind_group_layouts
+        .as_mut()
         .expect("ui bind group layouts should not be empty");
 
     bind_group_layouts.push(image_layout);
