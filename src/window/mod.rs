d297b225c6dda6543886694ecffd305e68a4b673
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-11-01 23:07:25 +0100

    remove old RenderContext

--- src/window/mod.rs
+++ src/window/mod.rs
@@ -1,10 +1,8 @@
 mod app_handler;
 pub mod config;
-mod render;
 mod state;
 
 pub(crate) use app_handler::AppHandler;
-pub(crate) use render::*;
 pub(crate) use state::*;
 
 /// Resource holding basic window state information.
