58cb7f2c70f59cbc6a5d61c5f693f701c2a035eb
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-12-10 16:41:03 +0100

    feat: add name component

--- src/assets/mod.rs
+++ src/assets/mod.rs
@@ -9,3 +9,22 @@ pub use loader::{AssetLoader, LoadableAsset};
 pub use shader::{Shader, ShaderLoader};
 
 pub trait Asset: Send + Sync + 'static {}
+
+/// Name component, mainly used for scene nodes but can be used as a standalone component to easily
+/// identify entities in the ECS
+#[derive(vavo_macros::Component, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
+pub struct Name(String);
+
+impl Name {
+    /// Create a new Name component
+    #[inline]
+    pub fn new<S: Into<String>>(name: S) -> Self {
+        Self(name.into())
+    }
+
+    /// Get the name string
+    #[inline]
+    pub fn name(&self) -> &str {
+        &self.0
+    }
+}
